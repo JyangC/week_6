64f6c8fe80120b37564ab3ad9736b2b8e2857df3
JyangC <93647241+JyangC@users.noreply.github.com>
2024-03-27 19:46:07 +0900

    Update HopingRabbitsGame.java

--- src/main/java/edu/postech/csed490h/HopingRabbitsGame.java
+++ src/main/java/edu/postech/csed490h/HopingRabbitsGame.java
@@ -102,7 +102,19 @@ public class HopingRabbitsGame {
 
         // Check if rabbit can jump (o/x and empty position in front of x/o)
         // And if rabbit can jump, jump and update game state
+        if (rabbit == Rabbit.X) {
+            movableRabbitPosition = currentGameState.indexOf("xo_");
+            if (tryUpdatingRabbitPosition(rabbit, movableRabbitPosition, movableRabbitPosition + 2)) {
+                return true;
+            }
+        } else {
+            movableRabbitPosition = currentGameState.indexOf("_xo");
+            if (tryUpdatingRabbitPosition(rabbit, movableRabbitPosition + 2, movableRabbitPosition)) {
+                return true;
+            }
+        }
 
+        
         return false;
     }
 
@@ -130,6 +142,11 @@ public class HopingRabbitsGame {
      * @return true if the game is stuck, false otherwise
      */
     boolean isStuck() {
+        String currState = this.getState();
+        String[] stateIfnotStuck = {"x_", "xo_", "_o", "_xo"};
+        for(String state : stateIfnotStuck){
+            if(currState.contains(state)){return false;}
+        }
         return true;
     }
 
@@ -141,6 +158,12 @@ public class HopingRabbitsGame {
      */
     String getState() {
         StringBuilder state = new StringBuilder();
+
+        for(Rabbit rabbit:gameState){
+            if(rabbit == Rabbit.X) {state.append("x");}
+            else if(rabbit == Rabbit.O) {state.append("o");}
+            else {state.append("_");}
+        }
 
         return state.toString();
     }
